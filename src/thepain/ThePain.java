8bcee35b4d2021f7ac3d85fe56816b062bd450fb
Cadel Monterde <cadel546@gmail.com>
2018-02-12 20:50:04 -0500

    update1

--- src/thepain/ThePain.java
+++ src/thepain/ThePain.java
@@ -19,51 +19,42 @@ public class ThePain {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static <AnyType extends Comparable<? super AnyType>> void main(String[] args) {
         
-        
-        difference();
-        
-    }
-
-    public static <AnyType extends Comparable<? super AnyType>> void difference() {
+       // List<AnyType> lol = new LinkedList<>();
         List<AnyType> L1 = new LinkedList<>();
         List<AnyType> L2 = new LinkedList<>();
         List<AnyType> Difference = new LinkedList<>();
-        AnyType itemL1 = null;
-        AnyType itemL2 = null;
-
         L1.add((AnyType) new Integer(1));
         L1.add((AnyType) new Integer(2));
         L1.add((AnyType) new Integer(3));
         L1.add((AnyType) new Integer(4));
-
+        
         L2.add((AnyType) new Integer(2));
         L2.add((AnyType) new Integer(6));
         L2.add((AnyType) new Integer(7));
         L2.add((AnyType) new Integer(8));
+        
+        difference(L1,L2,Difference);
+        
+    }
 
+    public static <AnyType extends Comparable<? super AnyType>> void difference(List<AnyType> L1, List<AnyType> L2,List<AnyType> Difference) {
+        AnyType itemL1 = null;
+        AnyType itemL2 = null;
+        
         ListIterator<AnyType> iterL1 = L1.listIterator();
         ListIterator<AnyType> iterL2 = L2.listIterator();
 
-        if (iterL1.hasNext() && iterL2.hasNext()) {
+        if (iterL1.hasNext() && iterL2.hasNext()) 
+        {
             itemL1 = iterL1.next();
             itemL2 = iterL2.next();
         }
-
-//        while (itemL1 != null) {
-//            int compareResult = itemL1.compareTo(itemL2);
-//            if (compareResult != 0) {
-//                Difference.add(itemL1);
-//                itemL1 = iterL1.hasNext() ? iterL1.next() : null;
-//                itemL2 = iterL2.hasNext() ? iterL2.next() : null;
-//            } else if (compareResult == 0) {
-//                itemL1 = iterL1.hasNext() ? iterL1.next() : null;
-//            }
-//        }
     int count = 0;
     int inc = 0;
     boolean state = false;
+    boolean did = true;
     while(count <= L1.size()-1)
     {
         
@@ -84,13 +75,14 @@ public class ThePain {
                 comp = itemL1.compareTo(itemL2);
                 
             }
-            if(comp != 0) 
+            if(comp != 0 && inc ==0) 
             {
                 //System.out.println("Here");
                 Difference.add(itemL1);
+                inc++;
                 itemL2 = iterL2.hasNext()?iterL2.next():null;
             }
-            else
+            else if(comp == 0)
             {
                 state = true;
             }
